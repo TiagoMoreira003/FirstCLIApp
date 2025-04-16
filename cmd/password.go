b48b950f077fdd4d02739329ea61bf9be4581a64
TiagoMoreira003 <tiagommoreira003@gmail.com>
2025-04-16 23:41:31 +0100

    Add upper case too

--- cmd/password.go
+++ cmd/password.go
@@ -19,20 +19,31 @@ func init(){
 	rootCmd.AddCommand(generateCmd)
 	generateCmd.Flags().IntP("length", "l", 4, "Length of the password")
 	generateCmd.Flags().BoolP("special-characters", "c", false, "Special characters (true/false)" )
+	generateCmd.Flags().BoolP("upper", "u", false, "Use uppercase letters")
 }
 
 func generatePassword(cmd *cobra.Command, args []string){
 	length, _ := cmd.Flags().GetInt("length")
-	specialChar, _ := cmd.Flags().GetBool("special-characters") 
+	specialChar, _ := cmd.Flags().GetBool("special-characters")
+	uppercase, _ := cmd.Flags().GetBool("upper")
+	
 	password := ""
 
 	var letters string
 
-	if !specialChar {
-		letters = "abcdefghijklmnopqrstuvwxyz"
-	}else {
-		letters = "abcdefghijklmnopqrstuvwxyz!@#$%&*-_"
-	}
+	lower := "abcdefghijklmnopqrstuvwxyz"
+	upper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	special := "!@#$%&*-_"
+
+	if uppercase && specialChar {
+		letters = lower + upper + special
+	} else if uppercase && !specialChar {
+		letters = lower + upper
+	} else if !uppercase && specialChar {
+		letters = lower + special
+	} else {
+		letters = lower
+}
 	// para o numero de length vai buscar uma letra do alfabeto
 	for i := 0; i<length; i++ {
 		randomIndex := rand.Intn(len(letters))
